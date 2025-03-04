0fb7d12bdbaf0d609e761b6136ecbd9770460b7a
Allegra Saggese <111875712+allegra-saggese-econ@users.noreply.github.com>
2025-03-04 13:28:40 -0800

    add to required package script
    - added packages for big memory storage to read IPUMS data in chunks
    - data requirement TOO HIGH for this

--- load-reqs.R
+++ load-reqs.R
@@ -4,5 +4,10 @@ library(utils) # base
 library(R.utils) # for unzipping
 library(ipumsr) # for reading in IPUMS data
 
+library(ff) # for loading IPMUS bc storage capacity is reached
+library(bigmemory)
+library(DBI) # for making IPUMS data into easily accesible database
+library(RSQLite) # for making IPUMS data lightweight database 
+
 library(tidyr)
 library(tidyverse)
